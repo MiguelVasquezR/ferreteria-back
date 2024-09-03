2c865f5f4ee47dbc361ff29a29466b3940dbda8e
MiguelVasquezR <mvasquesrosas@gmail.com>
2024-09-02 21:18:58 -0600

    vestion 1.0

---
+++ src/main/java/resenas/App.java
@@ -0,0 +1,36 @@
+package resenas;
+
+import static spark.Spark.*;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+        get("/", (req, res) -> "Hola esta es la ferreteria");
+
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+}

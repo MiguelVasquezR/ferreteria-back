d984cd96f92c4608ef7e459d3f825e212b65e708
MiguelVasquezR <mvasquesrosas@gmail.com>
2024-09-21 11:19:06 -0600

    create endpoint to get products

--- src/main/java/resenas/dao/DAOProducto.java
+++ src/main/java/resenas/dao/DAOProducto.java
@@ -2,20 +2,23 @@ package resenas.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import resenas.conexion.SQLConnection;
 import resenas.modelo.Producto;
 
 public class DAOProducto {
     private SQLConnection sqlConnection = new SQLConnection();
-    
-    public boolean agregarProducto(Producto producto){
+
+    public boolean agregarProducto(Producto producto) {
         Connection con = null;
         PreparedStatement ps = null;
-    
+
         try {
             con = sqlConnection.getConnection();
-            ps = con.prepareStatement("INSERT INTO producto (id_producto, url_image, codigo, nombre, cantidad, stock_minimo, costo, precio_menudeo, precio_mayoreo) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            ps = con.prepareStatement(
+                    "INSERT INTO producto (id_producto, url_image, codigo, nombre, cantidad, stock_minimo, costo, precio_menudeo, precio_mayoreo) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
             ps.setString(1, producto.getId());
             ps.setString(2, producto.getUrl_image());
             ps.setString(3, producto.getCodigo());
@@ -28,16 +31,15 @@ public class DAOProducto {
             int res = ps.executeUpdate();
             if (res > 0) {
                 return true;
-            }  else{
+            } else {
                 return false;
-            }          
-
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
             return false;
-            
-        }finally{
+
+        } finally {
             try {
                 con.close();
                 if (con.isClosed()) {
@@ -49,4 +51,44 @@ public class DAOProducto {
         }
 
     }
+
+    public ArrayList<Producto> getProducts() {
+        ArrayList<Producto> productos = new ArrayList<Producto>();
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            con = sqlConnection.getConnection();
+            ps = con.prepareStatement("SELECT * FROM producto");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Producto producto = new Producto();
+                producto.setId(rs.getString("id_producto"));
+                producto.setUrl_image(rs.getString("url_image"));
+                producto.setCodigo(rs.getString("codigo"));
+                producto.setNombre(rs.getString("nombre"));
+                producto.setCantidad(rs.getFloat("cantidad"));
+                producto.setStock_minimo(rs.getFloat("stock_minimo"));
+                producto.setCosto(rs.getFloat("costo"));
+                producto.setPrecio_menudeo(rs.getFloat("precio_menudeo"));
+                producto.setPrecio_mayoreo(rs.getFloat("precio_mayoreo"));
+                productos.add(producto);
+            }
+            return productos;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                con.close();
+                if (con.isClosed()) {
+                    sqlConnection.closeConnection();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }

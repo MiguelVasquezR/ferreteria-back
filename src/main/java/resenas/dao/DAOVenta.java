0782ba7e686311d4f792ef53e19e7826b5dcb595
NormaQuintana <NormaQuintana@gmail.com>
2024-10-07 18:13:01 -0600

    Create modelo venta

---
+++ src/main/java/resenas/dao/DAOVenta.java
@@ -0,0 +1,49 @@
+package resenas.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import resenas.conexion.SQLConnection;
+import resenas.modelo.Venta;
+
+public class DAOVenta {
+    private SQLConnection sqlConnection;
+    
+    public boolean guardarVenta(Venta venta){
+        sqlConnection = new SQLConnection();
+        Connection con = null;
+        PreparedStatement ps = null;
+        try {
+            con = sqlConnection.getConnection();
+            ps = con.prepareStatement(
+                    "INSERT INTO venta (idVenta, idUsuario, monto, fecha) VALUES(?, ?, ?, ?)");
+            ps.setString(1, venta.getIdVenta());
+            ps.setString(2, venta.getIdUsuario());
+            ps.setFloat(3, venta.getMonto());
+            ps.setDate(4, venta.getFecha());
+            
+            int res = ps.executeUpdate();
+            if (res > 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+
+        } finally {
+            try {
+                con.close();
+                if (con.isClosed()) {
+                    sqlConnection.closeConnection();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+    }
+    
+}
---
+++ src/main/java/resenas/modelo/Venta.java
@@ -0,0 +1,52 @@
+package resenas.modelo;
+
+import java.sql.Date;
+
+public class Venta {
+    private String idVenta;
+    private String idUsuario;
+    private float monto;
+    private Date fecha;
+    
+    public Venta() {
+
+    }
+
+    public String getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(String idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    
+
+    
+
+
+}

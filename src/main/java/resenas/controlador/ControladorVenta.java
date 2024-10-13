93ab233abd41d93d7aed82a788bc3f3a5d5f30c6
MiguelVasquezR <mvasquesrosas@gmail.com>
2024-10-13 08:35:41 -0600

    implemented save sale

--- src/main/java/resenas/controlador/ControladorVenta.java
+++ src/main/java/resenas/controlador/ControladorVenta.java
@@ -5,40 +5,31 @@ import java.text.SimpleDateFormat;
 import java.util.UUID;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 import resenas.dao.DAOVenta;
+import resenas.modelo.Producto_Venta;
 import resenas.modelo.Venta;
+import resenas.utils.JwtUtils;
 import spark.Request;
 import spark.Response;
-import java.sql.SQLException;
+
+import java.lang.reflect.Type;
 import java.text.ParseException;
-
-
 
 public class ControladorVenta {
     private static DAOVenta daoVenta = new DAOVenta();
     private static Gson gson = new Gson();
 
-    public static String guardarVenta(Request req, Response res){
-        Venta venta = gson.fromJson(req.body(), Venta.class);
-        venta.setIdVenta(UUID.randomUUID().toString());
-        System.out.println(venta.toString());
-        if (daoVenta.guardarVenta(venta)) {
-            return "Venta guardada exitosamente";
-        } else {
-            return "Error al agregar producto";
-        }
-
-    }
-
-
     public static String ventaDiaria(Request req, Response res) {
         // Obtener la fecha como String desde los parámetros
         String fechaString = req.queryParams("fecha");
-    
+
         // Formato de fecha esperado (yyyy-MM-dd, por ejemplo: "2024-10-07")
         SimpleDateFormat fechaFormato = new SimpleDateFormat("yyyy-MM-dd");
-    
+
         Date fechaUtil = null;
         try {
             // Convertir el String en un objeto Date de tipo java.util.Date
@@ -47,12 +38,11 @@ public class ControladorVenta {
             res.status(400); // Bad Request
             return "Formato de fecha incorrecto. Usa yyyy-MM-dd, por ejemplo: 2024-10-07";
         }
-    
+
         // Convertir java.util.Date a java.sql.Date para usarlo con la base de datos
         java.sql.Date fechaSql = new java.sql.Date(fechaUtil.getTime());
         System.out.println("Fecha enviada a la consulta: " + fechaSql);
 
-    
         // Verificar si existen ventas para la fecha especificada
         if (daoVenta.ventaDiaria(fechaSql) != null) {
             return gson.toJson(daoVenta.ventaDiaria(fechaSql));
@@ -61,5 +51,46 @@ public class ControladorVenta {
             return "No se han agregado productos para la fecha especificada.";
         }
     }
-    
+
+    public static String guardarVenta(Request req, Response res) {
+        System.out.println(req.body());
+        String token = req.headers("Authorization").replace("Bearer ", "");
+        String idUsuario = JwtUtils.obtenerIdUsuaraio(token);
+        Venta venta = new Venta();
+        venta.setIdVenta(UUID.randomUUID().toString());
+        venta.setIdUsuario(idUsuario);
+        venta.setCantidad(0);
+        venta.setFecha(java.sql.Date.valueOf(java.time.LocalDate.now()));
+        Type productoListType = new TypeToken<JsonObject>() {
+        }.getType();
+        JsonObject productosVenta = gson.fromJson(req.body(), productoListType);
+        venta.setMonto(productosVenta.get("total").getAsFloat());
+        if (!daoVenta.guardarVenta(venta)) {
+            return "Error al guardar la venta";
+        }
+        for (JsonElement productoElement : productosVenta.get("productos").getAsJsonArray()) {
+            JsonObject producto = productoElement.getAsJsonObject();
+            String cantidadCompra = producto.get("cantidadCompra").getAsString();
+            float cantidad = Float.parseFloat(cantidadCompra);
+
+            venta.setCantidad(venta.getCantidad() + cantidad);
+
+            Producto_Venta productoVenta = new Producto_Venta();
+            productoVenta.setIdProductoVenta(UUID.randomUUID().toString());
+            productoVenta.setIdProducto(producto.get("idProducto").getAsString());
+            productoVenta.setIdVenta(venta.getIdVenta());
+            productoVenta.setCantidad(cantidad);
+
+            if (!daoVenta.guardarProductoVenta(productoVenta)) {
+                return "No se pudo procesar la compra";
+            }
+        }
+
+        if (!daoVenta.actualizarVenta(venta)) {
+            return "Error al actualizar la cantidad de la venta";
+        }
+
+        return "Venta guardada exitosamente";
+    }
+
 }
--- src/main/java/resenas/dao/DAOVenta.java
+++ src/main/java/resenas/dao/DAOVenta.java
@@ -7,24 +7,28 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import resenas.conexion.SQLConnection;
+import resenas.modelo.Producto_Venta;
 import resenas.modelo.Venta;
 
 public class DAOVenta {
     private SQLConnection sqlConnection;
-    
-    public boolean guardarVenta(Venta venta){
+
+    public boolean guardarVenta(Venta venta) {
+        System.out.println(venta);
         sqlConnection = new SQLConnection();
         Connection con = null;
         PreparedStatement ps = null;
         try {
             con = sqlConnection.getConnection();
             ps = con.prepareStatement(
-                    "INSERT INTO VENTA (idVenta, idUsuario, monto, fecha) VALUES(?, ?, ?, ?)");
+                    "INSERT INTO VENTA (idVenta, idUsuario, monto, fecha, cantidadProductos) VALUES(?, ?, ?, ?,?)");
             ps.setString(1, venta.getIdVenta());
             ps.setString(2, venta.getIdUsuario());
             ps.setFloat(3, venta.getMonto());
             ps.setDate(4, venta.getFecha());
-            
+            ps.setFloat(5, venta.getCantidad());
+
+            System.out.println(ps);
             int res = ps.executeUpdate();
             if (res > 0) {
                 return true;
@@ -46,13 +50,47 @@ public class DAOVenta {
                 e.printStackTrace();
             }
         }
-        
     }
 
-    public ArrayList<Venta> ventaDiaria(Date fecha){
+    public boolean guardarProductoVenta(Producto_Venta pv) {
+        sqlConnection = new SQLConnection();
+        Connection con = null;
+        PreparedStatement ps = null;
+        try {
+            con = sqlConnection.getConnection();
+            ps = con.prepareStatement(
+                    "INSERT INTO PRODUCTO_VENTA (idProductoVenta, idProducto, idVenta, cantidad) VALUES(?, ?, ?, ?)");
+            ps.setString(1, pv.getIdProductoVenta());
+            ps.setString(2, pv.getIdProducto());
+            ps.setString(3, pv.getIdVenta());
+            ps.setFloat(4, pv.getCantidad());
+            int res = ps.executeUpdate();
+            if (res > 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+
+        } finally {
+            try {
+                con.close();
+                if (con.isClosed()) {
+                    sqlConnection.closeConnection();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ArrayList<Venta> ventaDiaria(Date fecha) {
         sqlConnection = new SQLConnection();
         PreparedStatement ps = null;
-        ArrayList<Venta>vDiaria = new ArrayList<Venta>();
+        ArrayList<Venta> vDiaria = new ArrayList<Venta>();
         Connection con = null;
         ResultSet rs = null;
         try {
@@ -60,11 +98,11 @@ public class DAOVenta {
             ps = con.prepareStatement("SELECT *FROM venta WHERE DATE(fecha) = ?");
             ps.setDate(1, fecha);
             rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 Venta venta = new Venta();
                 venta.setIdVenta(rs.getString("idVenta"));
                 venta.setIdUsuario(rs.getString("idUsuario"));
-                venta.setMonto(rs.getFloat("monto"));
+                venta.setCantidad(rs.getFloat("monto"));
                 venta.setFecha(rs.getDate("fecha"));
                 vDiaria.add(venta);
             }
@@ -72,7 +110,7 @@ public class DAOVenta {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
-        }finally{
+        } finally {
             try {
                 if (con.isClosed()) {
                     sqlConnection.closeConnection();
@@ -82,5 +120,39 @@ public class DAOVenta {
             }
         }
     }
-    
+
+    public boolean actualizarVenta(Venta venta) {
+        System.out.println(venta.toString());
+        sqlConnection = new SQLConnection();
+        Connection con = null;
+        PreparedStatement ps = null;
+        try {
+            con = sqlConnection.getConnection();
+            ps = con.prepareStatement("UPDATE VENTA SET monto = ?, cantidadProductos=? WHERE idVenta = ?");
+            ps.setFloat(1, venta.getMonto());
+            ps.setFloat(2, venta.getCantidad());
+            ps.setString(3, venta.getIdVenta());
+            int res = ps.executeUpdate();
+            if (res > 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+
+        } finally {
+            try {
+                con.close();
+                if (con.isClosed()) {
+                    sqlConnection.closeConnection();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
